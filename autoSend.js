a0cc66b057339fed8bb488561d187c795d667d9a
Zlkcyber <elankpasther@gmail.com>
2024-12-19 01:13:02 +0800

    first commit

--- autoSend.js
+++ autoSend.js
@@ -6,7 +6,7 @@ async function faucet() {
     log.warn(banner)
     const wallets = readWallets();
     log.info(`Found ${wallets.length} existing wallets...`);
-    const faucetAmount = await askQuestion('How Many ETH You Want To Send to each address, example 0.01 : ')
+    const faucetAmount = await askQuestion('How Many ETH You Want To Send to each address, example 0.0003 : ')
     const pvKey = await askQuestion('Please enter your private key that has ETH on Base Mainnet\nThis wallet will be used to send a fund to each address : ');
 
     for (let i = 0; i < wallets.length; i++) {
