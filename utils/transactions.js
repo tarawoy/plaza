a8569cb3067007b4936363651ca1fad1aab9691b
Luckyxg <87571231+GzGod@users.noreply.github.com>
2025-02-01 23:21:34 +0800

    Update transactions.js

--- utils/transactions.js
+++ utils/transactions.js
@@ -2,16 +2,17 @@ import { ethers } from 'ethers';
 import log from "./logger.js";
 
 // 配置
-const provider = new ethers.JsonRpcProvider('https://sepolia.base.org');
-const contractAddress = '0x47129e886b44B5b8815e6471FCD7b31515d83242';
+const provider = new ethers.JsonRpcProvider('https://base-sepolia-rpc.publicnode.com');
+const contractAddress = '0xF39635F2adF40608255779ff742Afe13dE31f577';
+const contractSwap = "0x809daBC75201F92AC40973f22db37995676BaA04"
 const explorer = 'https://sepolia.basescan.org/tx/'
 const ApproveAmount = ethers.parseUnits('10000', 'ether');
 const depositAmount = ethers.parseUnits('0.01', 'ether');
 const minAmount = ethers.parseUnits('0.00001', 'ether');
 const tokens = [
     { address: '0x13e5fb0b6534bb22cbc59fae339dbbe0dc906871', name: 'wstETH' },
-    { address: '0x1aC493C87a483518642f320Ba5b342c7b78154ED', name: 'bondETH' },
-    { address: '0x975f67319f9DA83B403309108d4a8f84031538A6', name: 'levETH' },
+    { address: '0x5Bd36745f6199CF32d2465Ef1F8D6c51dCA9BdEE', name: 'bondETH' },
+    { address: '0x98f665D98a046fB81147879eCBE9A6fF68BC276C', name: 'levETH' },
 ];
 
 // ERC20 ABI
@@ -65,25 +66,41 @@ const createABI = [
     },
 ];
 
-// 函数检查并批准代币
+// 检查和授权代币的函数
 const approveTokenIfNeeded = async (wallet, tokenAddress, tokenName) => {
     try {
         const tokenContract = new ethers.Contract(tokenAddress, erc20ABI, wallet);
-        const allowance = await tokenContract.allowance(wallet.address, contractAddress);
+        const allowance = await tokenContract.allowance(wallet.address, contractSwap);
 
         if (allowance > depositAmount) {
             return;
         }
 
-        const tx = await tokenContract.approve(contractAddress, ApproveAmount);
-        await tx.wait();
-        log.info(`${tokenName} 的批准交易已确认 ${explorer}${tx.hash}`);
+        const feeData = await provider.getFeeData();
+        const price = feeData.gasPrice;
+        const increasedGasPrice = (price * BigInt(125)) / BigInt(100);
+
+        const tx = await tokenContract.approve(contractSwap, ApproveAmount, {
+            gasPrice: increasedGasPrice
+        });
+
+        log.info(`授权交易已发送至 ${tokenName}: ${tx.hash}`);
+
+        const timeout = 60 * 1000;
+        const txPromise = tx.wait();
+
+        await Promise.race([
+            txPromise,
+            new Promise((_, reject) => setTimeout(() => reject(new Error('交易超时')), timeout))
+        ]);
+
+        log.info(`${tokenName} 的授权交易已确认: ${explorer}${tx.hash}`);
     } catch (error) {
-        log.error(`批准 ${tokenName} 时出错:`, error);
+        log.error(`在授权 ${tokenName} 时发生错误:`, error);
     }
 };
 
-// 更新的 approveAllTokens 函数
+// 更新过的 approveAllTokens 函数
 const approveAllTokens = async (wallet) => {
     for (const token of tokens) {
         await approveTokenIfNeeded(wallet, token.address, token.name);
@@ -93,28 +110,45 @@ const approveAllTokens = async (wallet) => {
 // 存款和赎回函数
 const deposit = async (contract, tokenType) => {
     try {
-        const tx = await contract.create(tokenType, depositAmount, minAmount);
-        await tx.wait();
-        log.info(`存款交易已确认 ${explorer}${tx.hash}`);
-        
-        // 增加随机间隔
-        const randomDelay = Math.floor(Math.random() * (200 - 15 + 1)) + 15;
-        log.info(`等待 ${randomDelay} 秒后执行赎回...`);
-        await new Promise(resolve => setTimeout(resolve, randomDelay * 1000));
+        const price = (await provider.getFeeData()).gasPrice
+        const increasedGasPrice = (price * BigInt(125)) / BigInt(100);
+        const tx = await contract.create(tokenType, depositAmount, minAmount, {
+            gasPrice: increasedGasPrice
+        });
+
+        const timeout = 60 * 1000;
+        const txPromise = tx.wait();
+
+        await Promise.race([
+            txPromise,
+            new Promise((_, reject) => setTimeout(() => reject(new Error('交易超时')), timeout))
+        ]);
+
+        log.info(`存款交易已确认: ${explorer}${tx.hash}`);
     } catch (error) {
-        log.error('存款过程中出错:', error);
+        log.error('存款时发生错误:', error);
     }
 };
 
 const redeem = async (contract, tokenType) => {
     try {
+        const price = (await provider.getFeeData()).gasPrice
+        const increasedGasPrice = (price * BigInt(125)) / BigInt(100);
         const tx = await contract.redeem(tokenType, depositAmount, minAmount, {
-            gasLimit: '0x493e0',
+            gasPrice: increasedGasPrice
         });
-        await tx.wait();
-        log.info(`赎回交易已确认. ${explorer}${tx.hash}`);
+
+        const timeout = 60 * 1000;
+        const txPromise = tx.wait();
+
+        await Promise.race([
+            txPromise,
+            new Promise((_, reject) => setTimeout(() => reject(new Error('交易超时')), timeout))
+        ]);
+
+        log.info(`赎回交易已确认: ${explorer}${tx.hash}`);
     } catch (error) {
-        log.error('赎回过程中出错:', error);
+        log.error('赎回时发生错误:', error);
     }
 };
 
